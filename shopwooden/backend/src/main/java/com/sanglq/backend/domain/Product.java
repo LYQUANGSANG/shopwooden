ebf2cf1c90f1d819fc8ba35e5bc958788e7f704f
ChungNT98 <91796314+ChungNT98@users.noreply.github.com>
2023-05-29 22:52:14 +0700

    Demo Product Domain

---
+++ shopwooden/backend/src/main/java/com/sanglq/backend/domain/Product.java
@@ -0,0 +1,20 @@
+package com.sanglq.backend.domain;
+
+public class Product {
+
+    private String id;
+
+    private String name;
+
+    private double price;
+
+    private String description;
+
+    public Product(String id, String name, double price, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
